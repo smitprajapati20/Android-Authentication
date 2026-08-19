77dc7f3635a22a19c8279ef009cf8f45696cc314
Smit Prajapati <prajaptismit28@gmail.com>
2026-08-19 16:04:29 +0530

    recyclerView complete

--- app/src/main/java/com/example/logsignjavadb/CategoryActivity.java
+++ app/src/main/java/com/example/logsignjavadb/CategoryActivity.java
@@ -9,11 +9,11 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 public class CategoryActivity extends AppCompatActivity {
 
-    int[] idArray = {1,2,3,4,5};
-    String[] nameArray = {"Electronics","Books","Cloths","Shoes","Bags"};
-    int[] imageArray = {R.drawable.electronics,R.drawable.books,R.drawable.clothes,R.drawable.shoes,R.drawable.bags};
+    int[] idArray = {1,2,3,4,5,6,7,8};
+    String[] nameArray = {"Electronics","Books","Cloths","Shoes","Bags","Bike","Car","Mobile"};
+    int[] imageArray = {R.drawable.electronics,R.drawable.books,R.drawable.clothes,R.drawable.shoes,R.drawable.bags,R.drawable.bike,R.drawable.car,R.drawable.mobile};
 
-    RecyclerView recyclerView;
+    RecyclerView category_recycler;
 
 
     @Override
@@ -22,11 +22,11 @@ public class CategoryActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_category);
 
-        recyclerView = findViewById(R.id.category_recycler);
+        category_recycler = findViewById(R.id.category_recycler);
 
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.HORIZONTAL));
+        category_recycler.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL));
 
-        CategoryAdapter adapter = new CategoryAdapter(CategoryActivity.this,idArray,nameArray,imageArray);
-        recyclerView.setAdapter(adapter);
+        CategoryAdapter adapter = new CategoryAdapter(CategoryActivity.this, idArray, nameArray, imageArray);
+        category_recycler.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/logsignjavadb/CategoryAdapter.java
+++ app/src/main/java/com/example/logsignjavadb/CategoryAdapter.java
@@ -1,6 +1,58 @@
 package com.example.logsignjavadb;
 
-public class CategoryAdapter {
-    public CategoryAdapter(CategoryActivity categoryActivity, int[] idArray, String[] nameArray, int[] imageArray) {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyHolder> {
+    Context context;
+    int[] idArray;
+    String[] nameArray;
+    int[] imageArray;
+
+    public CategoryAdapter(Context context, int[] idArray, String[] nameArray, int[] imageArray) {
+        this.context = context;
+        this.idArray = idArray;
+        this.nameArray = nameArray;
+        this.imageArray = imageArray;
     }
+
+    @NonNull
+    @Override
+    public CategoryAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_category,parent,false);
+        return new MyHolder(view);
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView name;
+
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.item_img);
+            name = itemView.findViewById(R.id.item_name);
+        }
+    }
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.MyHolder holder, int position) {
+        holder.image.setImageResource(imageArray[position]);
+        holder.name.setText(nameArray[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return idArray.length;
+    }
+
+
 }
+
+
